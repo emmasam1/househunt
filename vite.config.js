af3d240875aa0039977ee2cee3b3087995c54224
emmanuel sam <fndm4@gmail.com>
2026-06-02 21:11:26 +0100

    first push

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+// import babel from '@rolldown/plugin-babel'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  build: {
-    outDir: 'build', // 👈 this changes the output folder
-  },
+  plugins: [
+    react(),
+    tailwindcss()
+    // babel({ presets: [reactCompilerPreset()] })
+  ],
 })
